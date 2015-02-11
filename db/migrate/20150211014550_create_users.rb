6b373f1b151f836f6597750c5dff28d7706bcde5
Yatish Mehta <yatishmehta27@gmail.com>
2015-02-10 21:12:14 -0500

    Added project foreign key in User

--- db/migrate/20150211014550_create_users.rb
+++ db/migrate/20150211014550_create_users.rb
@@ -2,10 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.integer :role, default: 1
-
+      t.integer :role, default: 0
       t.references :project
-
       t.timestamps null: false
     end
   end
