6d2974efb55aba70eb03ebc0ff46a3ad02d4900c
Yatish Mehta <yatishmehta27@gmail.com>
2015-02-11 01:45:56 -0500

    Added propers views

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,7 @@ class Story < ActiveRecord::Base
   validates :name, presence: true
 
   belongs_to :project
-  belongs_to :signup_user, class_name: :user 
+  belongs_to :signup_user, class_name: "User",  foreign_key: "signup_user_id" 
 
   has_and_belongs_to_many :users
 end
