f7f6f57792d0254ecd36bfd6279f5b0215bb753f
Yatish Mehta <yatishmehta27@gmail.com>
2015-02-13 21:51:47 -0500

    Added stages

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,2 @@
 ROLES = {admin: 1, developer: 0, 1 => 'Admin', 0 => 'Developer'}
-STAGES = {'Analysis': 0, 'Ready for dev': 1, 'In dev': 2, 'Dev Complete': 3, 'In Test': 4, 'Complete': 5}
+STAGES = {'Analysis'=> 0, 'Ready for dev'=> 1, 'In dev'=> 2, 'Dev Complete'=> 3, 'In Test'=> 4, 'Complete'=> 5}
