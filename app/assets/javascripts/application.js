39dc65f9138f0d64d7b4954a721bb7634ec242e9
shubham90 <shubham.bhawsinka@gmail.com>
2015-02-13 20:45:55 -0500

    pass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,31 +17,3 @@
 //= require_tree .
 
 
-			
-$(document).ready(function(){
-    $("input[type=password]").keyup(function(){ 
-  if($("#user_password").val().length >= 8){
-      submit1.disabled=true;
-    $("#8char").removeClass("glyphicon-remove");
-    $("#8char").addClass("glyphicon-ok");
-    $("#8char").css("color","#00A41E");
-  }else{
-      submit1.disabled=true;
-    $("#8char").removeClass("glyphicon-ok");
-    $("#8char").addClass("glyphicon-remove");
-    $("#8char").css("color","#FF0004");
-  }
-  if(($("#user_password").val() == $("#user_password_confirmation").val()) && $("#user_password").val()!="" ){
-    submit1.disabled=false;
-    $("#pwmatch").removeClass("glyphicon-remove");
-    $("#pwmatch").addClass("glyphicon-ok");
-    $("#pwmatch").css("color","#00A41E");
-    
-  }else{
-    $("#pwmatch").removeClass("glyphicon-ok");
-    $("#pwmatch").addClass("glyphicon-remove");
-    $("#pwmatch").css("color","#FF0004");
-  }
-  });
-
-});
