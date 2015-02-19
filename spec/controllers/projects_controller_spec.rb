276282a333f3dc9c65913f2b450be19674ae8370
shubham90 <shubham.bhawsinka@gmail.com>
2015-02-18 19:27:16 -0500

    Project controller test added

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,7 +2,34 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   describe "index" do
-    it "" do
+  	it "populates the table of projects"
+    it "renders the :index template" do
+    	get :index
+    	response.should render_template :index
     end
+  end
+  describe "show" do
+  	it "captures the project id" do
+  		get :show, id: project_id
+  		assigns(:id).should eq(project_id)
+  	end
+    it "renders the :show template" do
+    	get :show, id: project_id
+    	response.should render_template :show
+    end
+  end
+  describe "new" do
+  	it "assigns a new project to @project" do
+  		get :new
+  	end
+    it "renders the :new template"
+  end
+  describe "create" do
+  	context "with valid credentials of admin" do
+  	it "saves new project to the database" do
+  		post :create, contact: Project.attributes_for(:id)
+  		response.should render_template :new
+  	end
+    it "redirects to the :index template"
   end
 end
